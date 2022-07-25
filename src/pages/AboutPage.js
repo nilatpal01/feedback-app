1006889bf07579dd787550f252691411122fc3aa
nilatpal01 <dasn0101@gmail.com>
2022-07-25 17:22:38 +0530

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h2>About This Project</h2>
+        <p>This is a React App to leave feedback about a product or service</p>
+        <p>version: 1.0.0</p>
+        <p>
+          <Link to="/">Back to Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
